25f222bc80d400513889755627e3e321c5eb7028
fatihk29 <fatihkrc7@gmail.com>
2021-04-28 21:12:23 +0300

    inline removed

---
+++ src/screens/Contact/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#c3fc',
+  },
+  text: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: '800',
+  },
+});
+export default styles;
